419cef71659900e3d068dd2cf343ab54f5f3d9b5
Maria <Salvito1956@gmail.com>
2023-04-03 07:55:28 -0300

    arreio

---
+++ app.js
@@ -0,0 +1,23 @@
+const frase = "estudando programação" // constante => rótulo
+console.log(frase)
+console.log('---------------------')
+const PI = 3.14
+console.log(PI)
+console.log('---------------------')
+
+let texto = "computação" // variável => não aceita sobrescrita
+console.log(texto)
+console.log('---------------------')
+texto = "computador"
+console.log(texto)
+console.log('---------------------')
+
+var texto2 = "teste variável" // variável => aceita sobrescrita
+console.log(texto2)
+console.log('---------------------')
+var texto2 = "nova frase"
+console.log(texto2)
+console.log('---------------------')
+
+texto3 = "tese de variável" // declaração feita pelo interpretador js
+console.log(texto3)
